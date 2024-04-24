0b0f4ede30560cd79e91295f68396ce734793868
Lewis Campbell <info@lewiscampbell.tech>
2024-04-25 09:44:28 +1200

    empty definitions with, basic tsconfig setup'

---
+++ README.md
@@ -0,0 +1,4 @@
+# Tranposrt Unit IDentifier
+
+Reference implementation for the TUID, a unique identifier for loads in the
+logistics industry.
---
+++ src/nlis.js
@@ -0,0 +1,17 @@
+const Identifier = (longitude, latitude, elevation, elevationType) => {
+	throw 'TODO'
+}
+
+const encodePoint = (longitude, latitude) => { throw 'TODO' }
+
+const encodeLongitude = longitude => { throw 'TODO '}
+
+const encodeLatitude = longitude => { throw 'TODO '}
+
+const encodeElevation = (elevation, elevationType) => { throw 'TODO' }
+
+const encodeStorey = elevation => { throw 'TODO' }
+
+const encodeGroundLevel = elevation => { throw 'TODO' }
+
+const normalizeEncoding = encoding => { throw 'TODO' }
