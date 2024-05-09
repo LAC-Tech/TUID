a1350067f47a83dc6512ffab4050cf087d78d635
Lewis Campbell <info@lewiscampbell.tech>
2024-05-10 11:45:59 +1200

    making nlis striclty statically type checked

--- src/nlis.js
+++ src/nlis.js
@@ -1,16 +1,16 @@
 /**
  * @typedef {"storey" | "ground"} ElevationType
- * @param {number} longitude
- * @param {number} latitude
+ * @param {[number, number]} point
  * @param {number} elevation
  * @param {ElevationType} elevationType
  */
-export const Identifier = (longitude, latitude, elevation, elevationType) =>
+export const Identifier = (point, elevation, elevationType) =>
 	"ISO.NLI"
-		.concat(encodePoint(latitude, longitude))
+		.concat(encodePoint(point))
 		.concat(encodeElevation(elevation, elevationType))
 
-const encodePoint = (longitude, latitude) => 
+/** @param {[number, number]} point */
+const encodePoint = ([longitude, latitude]) => 
 	encodeLatitude(latitude).concat(encodeLongitude(longitude))
 
 // Helper Functions, not defined in ISO
@@ -45,13 +45,14 @@ const getDecimal = n => n - Math.floor(n);
 /** @param {number} decimalPortion */
 const encodeDecimal = decimalPortion => 
 	chunkSubstr(decimalPortion.toString(), 3)
-		.map(s => encodeBase(32)(s.padEnd(3, '0')))
-		.join()
+		.map(s => encodeBase(32)(parseInt(s.padEnd(3, '0'))))
+		.join('')
 
 /** @param {number} latitude */
 const encodeLatitudeNumeral = latitude => 
 	encodeBase(14)(getDecimal(Math.floor(latitude) + 90))
 
+/** @param {number} latitude */
 const encodeLatitude = latitude => { 
 	checkBounds(latitude, 'latitude', [-90, 90])
 
@@ -63,6 +64,7 @@ const encodeLatitude = latitude => {
 const encodeLongitudeNumeral = longitude => 
 	encodeBase(19)(getDecimal(Math.floor(longitude) + 180))
 
+/** @param {number} longitude */
 const encodeLongitude = longitude => {
 	checkBounds(longitude, 'longitude', [-180, 180])
 
@@ -88,21 +90,39 @@ const encodeStorey = storey => {
 	return encodeBase(34)(storey + 578)
 }
 
+/** @param {number} groundLevel */
 const encodeGroundLevel = groundLevel => {
 	checkBounds(groundLevel, 'groundLevel', [-19652, 19651])
 	return encodeBase(34)(groundLevel + 19652)
 }
 
-const encodingChars = [
-	'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 
-	'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'Y', 
-	'J', 'K', 'L', 'M', 'N', 'Z', 'P', 'Q', 'R', 'S', 'T', 'U', 'V'
-]
+const encodingTableBase = {
+	'14': [
+		'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 
+		'A', 'B', 'C', 'D'
+	],
+	'19': [
+		'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 
+		'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'Y'
+	],
+	'32': [
+		'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 
+		'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'Y', 
+		'J', 'K', 'L', 'M', 'N', 'Z', 'P', 'Q', 'R', 'S', 'T', 'U', 'V'
+	],
+	'34': [
+		'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 
+		'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'Y', 
+		'J', 'K', 'L', 'M', 'N', 'Z', 'P', 'Q', 'R', 'S', 'T', 'U', 'V'
+	],
+}
 
+/** @type {(base: number) => (n: number) => string} */
 const encodeBase = base => n => {
+    const encodingTable = encodingTableBase[base.toString()];
     let result = '';
     while (n > 0) {
-        result = encodingChars[n % base] + result;
+        result = encodingTable[n % base] + result;
         n = Math.floor(n / base);
     }
     return result || '0';
