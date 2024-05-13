b28219e1dcea4cd5e5f6d3f2dc6852fb425613dd
Lewis Campbell <info@lewiscampbell.tech>
2024-05-13 16:23:03 +1200

    made a point object, reordered functions as they appear in ISO docs, wrote some implementation notes at top of NLIS file

--- src/nlis.js
+++ src/nlis.js
@@ -1,6 +1,14 @@
 /**
+ * NOTES:
+ * - Writing these in the order defined in ISO 8000-118
+ * - ISO inconsistent wrt lat long OR long lat. Point object renders it moot.
+ */
+
+/**
  * @typedef {"storey" | "ground"} ElevationType
- * @param {[number, number]} point
+ * @typedef {{latitude: number, longitude: number}} Point
+ *
+ * @param {Point} point
  * @param {number} elevation
  * @param {ElevationType} elevationType
  */
@@ -9,9 +17,63 @@ export const Identifier = (point, elevation, elevationType) =>
 		.concat(encodePoint(point))
 		.concat(encodeElevation(elevation, elevationType))
 
-/** @param {[number, number]} point */
-const encodePoint = ([longitude, latitude]) => 
+/** @param {Point} point */
+const encodePoint = ({latitude, longitude}) => 
 	encodeLatitude(latitude).concat(encodeLongitude(longitude))
+
+/** @param {number} latitude */
+const encodeLatitude = latitude => { 
+	checkBounds(latitude, 'latitude', [-90, 90])
+
+	return encodeLatitudeNumeral(latitude)
+		.concat(encodeDecimal(getDecimal(latitude)))
+}
+
+/** @param {number} latitude */
+const encodeLatitudeNumeral = latitude => 
+	encodeBase(14)(getDecimal(Math.floor(latitude) + 90))
+
+/** @param {number} longitude */
+const encodeLongitude = longitude => {
+	checkBounds(longitude, 'longitude', [-180, 180])
+
+	return encodeLongitudeNumeral(longitude)
+		.concat(encodeDecimal(getDecimal(longitude)))
+}
+
+/** @param {number} longitude */
+const encodeLongitudeNumeral = longitude => 
+	encodeBase(19)(getDecimal(Math.floor(longitude) + 180))
+
+/** @type {(elevation: number, elevationType: ElevationType) => string} */
+const encodeElevation = (elevation, elevationType) => {
+	if (elevationType === 'storey') {
+		return encodeStorey(elevation)
+	} else if (elevationType === 'ground') {
+		return encodeGroundLevel(elevation)
+	} else {
+		throw new Error('unknown elevation type')
+	}
+}
+
+/** @param {number} storey */
+const encodeStorey = storey => {
+	// TODO: where are these numbers from?
+	checkBounds(storey, 'storey', [-578, 577])
+	return encodeBase(34)(storey + 578)
+}
+
+/** @param {number} groundLevel */
+const encodeGroundLevel = groundLevel => {
+	checkBounds(groundLevel, 'groundLevel', [-19652, 19651])
+	return encodeBase(34)(groundLevel + 19652)
+}
+
+/** @param {number} decimalPortion */
+const encodeDecimal = decimalPortion => 
+	chunkSubstr(decimalPortion.toString(), 3)
+		.map(s => encodeBase(32)(parseInt(s.padEnd(3, '0'))))
+		.join('')
 
 // Helper Functions, not defined in ISO
 
@@ -42,84 +104,31 @@ const checkBounds = (value, paramName, [min, max]) => {
 /** @param {number} n */
 const getDecimal = n => n - Math.floor(n);
 
-/** @param {number} decimalPortion */
-const encodeDecimal = decimalPortion => 
-	chunkSubstr(decimalPortion.toString(), 3)
-		.map(s => encodeBase(32)(parseInt(s.padEnd(3, '0'))))
-		.join('')
-
-/** @param {number} latitude */
-const encodeLatitudeNumeral = latitude => 
-	encodeBase(14)(getDecimal(Math.floor(latitude) + 90))
-
-/** @param {number} latitude */
-const encodeLatitude = latitude => { 
-	checkBounds(latitude, 'latitude', [-90, 90])
-
-	return encodeLatitudeNumeral(latitude)
-		.concat(encodeDecimal(getDecimal(latitude)))
-}
-
-/** @param {number} longitude */
-const encodeLongitudeNumeral = longitude => 
-	encodeBase(19)(getDecimal(Math.floor(longitude) + 180))
-
-/** @param {number} longitude */
-const encodeLongitude = longitude => {
-	checkBounds(longitude, 'longitude', [-180, 180])
-
-	return encodeLongitudeNumeral(longitude)
-		.concat(encodeDecimal(getDecimal(longitude)))
-}
-
-/** @type {(elevation: number, elevationType: ElevationType) => string} */
-const encodeElevation = (elevation, elevationType) => {
-	if (elevationType === 'storey') {
-		return encodeStorey(elevation)
-	} else if (elevationType === 'ground') {
-		return encodeGroundLevel(elevation)
-	} else {
-		throw new Error('unknown elevation type')
-	}
-}
-
-/** @param {number} storey */
-const encodeStorey = storey => {
-	// TODO: where are these numbers from?
-	checkBounds(storey, 'storey', [-578, 577])
-	return encodeBase(34)(storey + 578)
-}
-
-/** @param {number} groundLevel */
-const encodeGroundLevel = groundLevel => {
-	checkBounds(groundLevel, 'groundLevel', [-19652, 19651])
-	return encodeBase(34)(groundLevel + 19652)
-}
-
+// TODO: just do a slice of one array?
 const encodingTableBase = {
-	'14': [
+	14: [
 		'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 
 		'A', 'B', 'C', 'D'
 	],
-	'19': [
+	19: [
 		'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 
 		'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'Y'
 	],
-	'32': [
+	32: [
 		'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 
 		'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'Y', 
 		'J', 'K', 'L', 'M', 'N', 'Z', 'P', 'Q', 'R', 'S', 'T', 'U', 'V'
 	],
-	'34': [
+	34: [
 		'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 
 		'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'Y', 
 		'J', 'K', 'L', 'M', 'N', 'Z', 'P', 'Q', 'R', 'S', 'T', 'U', 'V'
 	],
 }
 
-/** @type {(base: number) => (n: number) => string} */
+/** @type {(base: 14 | 19 | 32 | 34) => (n: number) => string} */
 const encodeBase = base => n => {
-    const encodingTable = encodingTableBase[base.toString()];
+    const encodingTable = encodingTableBase[base];
     let result = '';
     while (n > 0) {
         result = encodingTable[n % base] + result;
--- src/nlis.test.js
+++ src/nlis.test.js
@@ -2,6 +2,7 @@ import { expect, test } from 'vitest'
 import { Identifier } from './nlis'
 
 test("Embassy of New Zealand in Washington DC", () => {
-	const actual = Identifier([38.918966, -77.064241], 0, 'ground')
-	expect(actual).toBe("ISONLI92SMU6-58207H-H0")
+	const actual = 
+		Identifier({latitude: 38.918966, longitude: -77.064241}, 0, 'ground')
+	expect(actual).toBe("ISO.NLI92SMU6-58207H-H0")
 })
