e987121a8a861ad54782a6320f5f86b860546529
Lewis Campbell <info@lewiscampbell.tech>
2024-06-06 21:38:53 +1200

    added my local to test cases, works too

--- src/nlis.test.js
+++ src/nlis.test.js
@@ -1,7 +1,14 @@
 import { expect, test } from 'vitest'
 import { Identifier } from './nlis'
 
+// Testing against data from https://e-nli.org/
+
 test("Embassy of New Zealand in Washington DC", () => {
 	const actual = Identifier({lat: 38.918966, long: -77.064241}, 0, 'storey')
 	expect(actual).toBe("ISO.NLI92SMU6-58207H-H0")
+})
+
+test("Ikamatua Hotel", () => {
+	const actual = Identifier({lat: -42.271374, long: 171.684597}, 0, 'storey')
+	expect(actual).toBe("ISO.NLI368FBM-Y9LCYL-H0")
 })
